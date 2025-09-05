6bb38ff7c347cbc6a4113d7b695fe79acb81df67
sheldcoop <106275963+sheldcoop@users.noreply.github.com>
2025-09-05 15:26:59 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@
 # --- Style Theme: Post-Etch AOI Panel ---
 # This palette is designed to look like a copper-clad panel from the PCB/IC Substrate industry.
 
-PANEL_COLOR = '#F4A460'
+PANEL_COLOR = '#E58A60'
 GRID_COLOR = 'black'
 BACKGROUND_COLOR ='white'
 PLOT_AREA_COLOR = '#F4A460'
