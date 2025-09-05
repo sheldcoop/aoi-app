dd63e6eb5681e7ba61557e772c6dc4a155d67a74
sheldcoop <106275963+sheldcoop@users.noreply.github.com>
2025-09-05 14:24:45 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@
 
 PANEL_COLOR = '#8B4513'
 GRID_COLOR = 'black'
-BACKGROUND_COLOR = '#0E1117'
+BACKGROUND_COLOR = 'white'
 PLOT_AREA_COLOR = '#F4A460'
 TEXT_COLOR = 'black'       # White text for readability on the dark background.
 
